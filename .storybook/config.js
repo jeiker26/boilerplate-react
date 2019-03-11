06e9afac030bbf8c67602a726c3dcd8c6b26a120
jesus.javega <jesusgraficap@gmail.com>
2019-03-11 17:46:02 +0100

    storiesbook dynamic config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,9 @@
-import { configure } from "@storybook/react";
+import { configure } from '@storybook/react';
 
-function requireAll(requireContext) {
-    return requireContext.keys().map(requireContext);
-  }
-  
+const req = require.context('../src/', true, /\.stories\.js$/);
+
 function loadStories() {
-    requireAll(require.context("../src/", true, /.story\.js?$/));
+  req.keys().forEach(filename => req(filename));
 }
-  
+
 configure(loadStories, module);
