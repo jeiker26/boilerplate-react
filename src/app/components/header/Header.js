c7f851d739a6e116387cfcb1d05b47d8c9d9a4c4
jesus.javega <jesusgraficap@gmail.com>
2019-03-16 23:16:41 +0100

    config cucumber

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -16,24 +16,24 @@ export const Header = () => {
       <nav className="navbar">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link data-cy={`${idComponent}-link-home`} to="/">
+            <Link data-test-id={`${idComponent}-link-home`} to="/">
               {t("home-title")}
             </Link>
           </li>
           <li className="nav-item">
-            <Link data-cy={`${idComponent}-link-users`} to="/users">
+            <Link data-test-id={`${idComponent}-link-users`} to="/users">
               {t("users-title")}
             </Link>
           </li>
           <li className="nav-item">
-            <Link data-cy={`${idComponent}-link-hello`} to="/hello">
+            <Link data-test-id={`${idComponent}-link-hello`} to="/hello">
               {t("hello-title")}
             </Link>
           </li>
           <li className="nav-item">
             {
               <span
-                data-cy={`${idComponent}-change-language-es`}
+                data-test-id={`${idComponent}-change-language-es`}
                 onClick={() => handleChangeLanguage("es")}
               >
                 {t("language-title-es")}
@@ -41,7 +41,7 @@ export const Header = () => {
             }
             {
               <span
-                data-cy={`${idComponent}-change-language-en`}
+                data-test-id={`${idComponent}-change-language-en`}
                 onClick={() => handleChangeLanguage("en")}
               >
                 {t("language-title-en")}
